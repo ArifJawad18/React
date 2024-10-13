ed9c1fec93733e61bacfee08232334a935882371
Arifjawad18 <arif.jawad18@gmail.com>
2024-10-13 14:01:39 +0600

    ract state change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("button clicked");
+  }
   return (
     <>
-    <h3>Name</h3>
-   
-      
+      <h3>React core concepts</h3>
+      <button onClick={handleClick}>Click Me</button>
     </>
   );
 }
-
 
 export default App;
