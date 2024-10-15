8964b12b31f67ed5c471f78e48fb909c546fcd89
Arifjawad18 <arif.jawad18@gmail.com>
2024-10-15 13:57:46 +0600

    ract useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-
 import Counter from "./Counter";
+import Users from "./Users";
+
 function App() {
   function handleClick() {
     alert("button clicked");
   }
   return (
     <>
+      <Users></Users>
       <Counter></Counter>
 
       <h3>React core concepts</h3>
